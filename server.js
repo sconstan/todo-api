df7c3fe0df861ad4f5265909986118deba53234b
Savvas Konstantinidis <sconstan@gmail.com>
2016-02-09 21:05:40 +0200

    just for sure

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Meet mom for lunch',
+	completed: false
+}, {
+	id: 2,
+	description: 'Goto market',
+	completed: false
+}, {
+	id: 3,
+	description: 'Buy milk',
+	completed: true
+}];
 
 app.get('/', function(req,res) {
 	res.send('Todo API Root');
 });
 
+// get /todos
+app.get('/todos', function(req,res) {
+	res.json(todos);
+});
+
+// get /todos/:id
+
 app.listen(PORT, function () {
 	console.log('Express listening on port: ' + PORT + '!');
-})
+});
