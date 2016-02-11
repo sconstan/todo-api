033e7139ea24a018db6dd187e054ecfb7a36c40e
Savvas Konstantinidis <sconstan@gmail.com>
2016-02-11 20:15:40 +0200

    refactored using underscore

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _=require('underscore');
+
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -22,27 +24,27 @@ app.get('/todos', function (req,res) {
 app.get ('/todos/:id', function (req,res) {
 
 	var todoid = parseInt(req.params.id, 10);
-//				 ===========================
-	var foundit;
 
-	todos.forEach (function (todoitem) {
-	
-		if (todoitem.id === todoid) {
-			foundit = todoitem;
-			res.json(todoitem);
-		}
-	});
+	var foundit = _.findWhere(todos, {id: todoid});
 
-	if (!foundit) {
+	if (foundit) {
+		res.json(foundit);
+	} else {
 	 	res.status(404).send();
-//		=======================
 	}
 });
 
 // POST /todos
 app.post('/todos', function (req,res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
 
+	if ((!_.isBoolean(body.completed)) || (!_.isString(body.description)) || (body.description.trim().length === 0)) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
+
+//	add id field to body
 	body.id = todoNextId++;
 
 //	add the Todo to the array...
@@ -51,6 +53,10 @@ app.post('/todos', function (req,res) {
 	res.json(body);
 
 });
+
+
+// delete a todo by id 
+
 
 app.listen(PORT, function () {
 	console.log('Express listening on port: ' + PORT + '!');
