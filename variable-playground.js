f45b5c3ce3017e78259ec781eb03ede90df70293
Savvas Konstantinidis <sconstan@gmail.com>
2016-02-13 18:49:25 +0200

    added variable-playground/js

---
+++ variable-playground.js
@@ -0,0 +1,31 @@
+// var person = {
+// 	name: 'Savvas',
+// 	age: 40
+// };
+
+
+// function updatePerson (obj) {
+// // 	obj = {
+// // 		name: 'Savva',
+// // 		age: 46
+// // 	};
+// // 
+// obj.age = 46;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+
+grades = [ 15, 88 ];
+
+function addGrades (grades) {
+	grades.push(50);
+	debugger;
+
+	// grades = [12, 50,88];
+}
+
+addGrades(grades);
+console.log(grades);
