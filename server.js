7c5ae571de6a0eed1c6c2e0ded35aa84088ec21e
Savvas Konstantinidis <sconstan@gmail.com>
2016-02-28 22:48:35 +0200

    added DELETE/user/login

--- server.js
+++ server.js
@@ -144,18 +144,6 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	}, function (e) {
 		res.status(500).send();
 	});
-	
-	// var foundit = _.findWhere(todos, {
-	// 	id: todoid
-	// });
-
-	// todos = _.without(todos, foundit);
-	// if (foundit) {
-	// 	res.json('Deleted todo item: ' + foundit.id + ' with description ' + foundit.description);
-	// } else {
-	// 	res.json('Item not found');
-	// }
-
 });
 
 // PUT /todos/:id
@@ -218,28 +206,41 @@ app.post('/users', function (req,res) {
 // POST /users/login
 app.post('/users/login', function(req,res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.authenticate(body).then (function (user) {
 		var token = user.generateToken('authentication');
+		userInstance = user;
 
-		// send back a token
-		if (token) {
-			res.header('Auth', token).json(user.toPublicJSON());
-		} else {
-			res.status(401).send();
-		}
-		
-	}, function (e) {
+		return db.token.create({
+			token: token
+		});
+
+	}).then (function (tokenInstance) {
+
+//console.log(tokenInstance.get('token'));	
+
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch (function (e) {
 		res.status(401).send();
 	});
  
 });
 
+//DELETE /users/login
+app.delete('/users/login', middleware.requireAuthentication, function (req,res) {
+
+	req.token.destroy().then(function () {
+		res.status(204).send();
+	}).catch(function () {
+		res.status(500).send();
+	});
+});
 
 
 // synchronize the DB
 db.sequelize.sync({
-	// force: true
+	force: true
 }).then(function () {
 	app.listen(PORT, function() {
 		console.log('Express listening on port: ' + PORT + '!');
